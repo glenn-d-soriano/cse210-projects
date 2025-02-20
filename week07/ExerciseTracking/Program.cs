2901f59eeab3c20ba2cb5e2d5fbd6ef0bb57c60c
glenn-d-soriano <glenndannielsoriano@gmail.com>
2025-02-19 22:55:25 -0600

    Exercise Tracking Program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11,3 ), 30, 2.0),
+            new Cycling(new DateTime(2022, 11,3 ), 30, 15.0),
+            new Swimming(new DateTime(2022, 11,3 ), 30, 20),
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.Writeline(activity.GetSummary());
+        }
+
+
     }
 }
